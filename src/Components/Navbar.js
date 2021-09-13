cb43523b86531bd07e148125227bb948bb8736aa
Muhammad Zunnoorain <zunnoorain002@gmail.com>
2021-09-14 03:00:22 +0500

    new commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,63 +1,83 @@
 import React from "react";
+// import ColorTempletes from "./ColorTempletes";
+import "./colortemplete.css";
 import { Link } from "react-router-dom";
-import "./style.css"
-function Navbar(props) {
-  
 
+export default function Navbar(props) {
+
+  function changeTitle() {
+    document.title= 'TextUtils - Home';
+  }
+
+  
   return (
-    <>
+    <div>
       <nav
-        id="navbar_top"
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Webonologist
+            {props.title}
           </Link>
           <button
             className="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/home">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/aboutus">
-                  About Us
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/services">
-                  Services
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contactus">
-                  Contact Us
+                <Link className="nav-link" to="/about">
+                  {props.abouttext}
                 </Link>
               </li>
             </ul>
-          </div>
-
+            {/* <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Search
+              </button>
+            </form> */}
+            
+            
           
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="flexSwitchCheckDefault"
+                onClick={props.toggleMode}
+              />
+              <label
+                className="form-check-label"
+                htmlFor="flexSwitchCheckDefault"
+              >
+                {props.modeText}
+              </label>
+            </div>
+          </div>
         </div>
       </nav>
-
-      {/* {bootstrapFixed()} */}
-    </>
+    </div>
   );
 }
-
-export default Navbar;
