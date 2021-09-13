7b6e05c7c51183cfda373f2e9e85ff8c47d2b1d6
Muhammad Zunnoorain <zunnoorain002@gmail.com>
2021-09-14 00:29:10 +0500

    new edit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+// import NavbarList from "./NavbarList";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
-import ContactUs from "./Components/ContactUs";
-import About from "./Components/Aboutus/About";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomePage from "./Components/Home/HomePage";
-import OurService from "./Components/Services/OurService";
-import Chatbot from "./Components/Services/Chatbot";
-import Contactpage from "./Components/Contactus/Contactpage";
+import TextForm from "./Components/TextForm";
+// import TextForm from "./Components/Textfromed";
+import Alert from "./Components/Alert";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [modeText, setmodeText] = useState("Enable Dark Mode");
+  const [alert, setAlert] = useState(null);
+  
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      setmodeText("Enable Dark Mode");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode hase been enabled", "success");
+
+      document.title = "TextUtils - Light Mode";
+    } else {
+      setMode("dark");
+      setmodeText("Enable Light Mode");
+      document.body.style.backgroundColor = "rgba(18,18,18,255)      ";
+      showAlert("Dark Mode hase been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    }
+  };
+
+
   return (
     <>
-      
-      <Router>
-        <Navbar />
+
+    <Router>
+      <Navbar
+        title="Text Utils"
+        abouttext="About Us"
+        modeText={modeText}
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+
+      <Alert alert={alert}/>
+
+      {/* <TextForm showAlert={showAlert} heading="Enter Your Text Bellow" mode={mode}/> */}
+
+      <div className="container my-3">
         <Switch>
-          <Route path="/home">
-            <HomePage/>
+          <Route path="/about">
+            <About /> 
           </Route>
-
-          <Route path="/aboutus">
-            <About title={"About Us"}/>
+          <Route path="/">
+          <TextForm showAlert={showAlert} heading="Enter Your Text Bellow" mode={mode}/>
           </Route>
-
-          <Route path="/services">
-            <OurService />
-          </Route>
-
-
-          <Route path="/contactus">
-            <Contactpage />
-          </Route>
-
         </Switch>
+      </div>
       </Router>
-      <Chatbot />
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,78 +1,83 @@
-import React, { useState } from "react";
+import React from "react";
+// import ColorTempletes from "./ColorTempletes";
+import "./colortemplete.css";
 import { Link } from "react-router-dom";
-import "./style.css"
-function Navbar(props) {
-  function bootstrapFixed() {
-     document.addEventListener("DOMContentLoaded", function () {
-       window.addEventListener("scroll", function () {
-         if (window.scrollY > 50) {
-           document.getElementById("navbar_top").classList.add("fixed-top");
-           // add padding top to show content behind navbar
-           var navbar_height = document.querySelector(".navbar").offsetHeight;
-           document.body.style.paddingTop = navbar_height + "px";
-         } else {
-           document.getElementById("navbar_top").classList.remove("fixed-top");
-           // remove padding top from body
-           document.body.style.paddingTop = "0";
-         }
-       });
-     });
-   }
 
+export default function Navbar(props) {
+
+  function changeTitle() {
+    document.title= 'TextUtils - Home';
+  }
+
+  
   return (
-    <>
+    <div>
       <nav
-        id="navbar_top"
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container">
-          <Link className="navbar-brand" to="/home">
-            Webonologist
+          <Link className="navbar-brand" to="/">
+            {props.title}
           </Link>
           <button
             className="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/home">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/aboutus">
-                  About Us
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/services">
-                  Services
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contactus">
-                  Contact Us
+                <Link className="nav-link" to="/about">
+                  {props.abouttext}
                 </Link>
               </li>
             </ul>
-          </div>
-
+            {/* <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Search
+              </button>
+            </form> */}
+            
+            
           
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="flexSwitchCheckDefault"
+                onClick={props.toggleMode}
+              />
+              <label
+                className="form-check-label"
+                htmlFor="flexSwitchCheckDefault"
+              >
+                {props.modeText}
+              </label>
+            </div>
+          </div>
         </div>
       </nav>
-
-      {bootstrapFixed()}
-    </>
+    </div>
   );
 }
-
-export default Navbar;
